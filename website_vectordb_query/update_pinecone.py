4213e05de7dfe0b2e9187c242a31ea7903f2908f
Diana Wang <107273800+DianaWang1110@users.noreply.github.com>
2024-10-06 21:48:52 -0400

    Delete website_vectordb_query directory

--- website_vectordb_query/update_pinecone.py
+++
@@ -1,8 +0,0 @@
-from constants import *
-import pinecone
-
-pinecone.deployment.api_key = pinecone_api_key
-
-def update_pinecone(link, title, content_type):
-    if content_type == 'pdf':
-        
